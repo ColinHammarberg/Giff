2aa9ccb693a94c415763d17d71d9c7ccf827193a
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-08-24 19:59:10 +0200

    update download fuction etc.

--- giff-frontend/src/App.js
+++ giff-frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { TextField } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 import axios from 'axios';
 import Gif from './gifs/scrolling_animation.gif'
 import './App.css'
 
 function App() {
   const [gifGenerated, setGifGenerated] = useState(false);
+  const [generatedGifUrl, setGeneratedGifUrl] = useState('');
   const [url, setUrl] = useState('');
 
   const generateGif = async () => {
@@ -13,6 +14,7 @@ function App() {
       const response = await axios.post('http://127.0.0.1:5000/generate-gif', {url});
       console.log('response', response);
       if (response.data.message === 'GIF generated successfully') {
+        setGeneratedGifUrl(Gif);
         setGifGenerated(true);
       }
     } catch (error) {
@@ -20,17 +22,37 @@ function App() {
     }
   };
 
+  function handleDownloadClick() {
+    if (gifGenerated) {
+      // Create a virtual anchor element and trigger the download
+      const link = document.createElement('a');
+      link.href = generatedGifUrl; // Use the actual URL here
+      link.target = '_blank';
+      link.download = 'generated_gif.gif';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+
   function handleOnChangeUrl(event) {
     setUrl(event?.target?.value)
   }
 
-  console.log('url', url);
-
   return (
     <div className="App">
-      <h1>Interactive GIF Generator</h1>
-      <TextField onChange={(event) => handleOnChangeUrl(event)} />
-      <button onClick={generateGif}>Generate GIF</button>
+      <Box className="header-content">
+        <div className="header">Gif-t</div>
+        <div className="sub-header">Give the perfect gif</div>
+      </Box>
+      <Box className="text-field-content">
+        <div className="text-field-header">Add any* url</div>
+        <TextField onChange={(event) => handleOnChangeUrl(event)} />
+      </Box>
+      <Box className="btn-content">
+        <Button className="action-btn" onClick={generateGif}>Create GIF</Button>
+        {gifGenerated && <Button className="action-btn" onClick={handleDownloadClick}>Download GIF</Button>}
+      </Box>
       <div className="gifs">
         {gifGenerated && <img src={Gif} alt="Generated GIF" />}
       </div>
--- test/app.py
+++ test/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, send_file
 from selenium import webdriver
 import imageio
 import time
@@ -13,9 +13,10 @@ def index():
     return render_template('index.html')
 
 @app.route('/generate-gif', methods=['POST'])
+
 def generate_gif():
     data = request.get_json()
-    URL = data.get('url')  # Get the URL from the request JSON
+    URL = data.get('url') # Get the URL from the request JSON
 
     # Create a new Chrome session
     driver = webdriver.Chrome()
@@ -44,7 +45,7 @@ def generate_gif():
     output_path = os.path.join(gifs_folder, 'scrolling_animation.gif')
     screenshots = sorted(os.listdir(screenshots_dir))
     frames = [imageio.imread(os.path.join(screenshots_dir, screenshot)) for screenshot in screenshots]
-    imageio.mimsave(output_path, frames, duration=0.6)
+    imageio.mimsave(output_path, frames, duration=0.06, loop=0) # Set duration and loop
     print(f"GIF saved at {output_path}")
 
     # Clean up: Delete individual screenshots
@@ -53,6 +54,14 @@ def generate_gif():
     os.rmdir(screenshots_dir)
 
     return jsonify({'message': 'GIF generated successfully'})
+
+@app.route('/download-gif', methods=['GET'])
+
+def download_gif():
+    gifs_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'giff-frontend', 'src', 'gifs')
+    gif_filename = 'scrolling_animation.gif'
+    gif_path = os.path.join(gifs_folder, gif_filename)
+    return send_file(gif_path, as_attachment=True, attachment_filename=gif_filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
