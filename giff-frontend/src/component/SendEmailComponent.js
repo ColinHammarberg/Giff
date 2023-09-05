7de40b9a479a7aca3366fe4cf0d2301175e5b79e
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-09-05 20:55:40 +0200

    update context and new page

--- giff-frontend/src/component/SendEmailComponent.js
+++ giff-frontend/src/component/SendEmailComponent.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box, Button, TextField } from '@mui/material';
 import './SendEmailComponent.scss';
 import Header from './Header';
+import { GiftContext } from '../context/GiftContextProvider';
 
 function SendEmailComponent(props) {
-  const { onChange, gifGenerated, onKeyPress } = props;
+  const { onChange } = useContext(GiftContext); // Get the context value
 
-  function handleOnChangeUrl(event) {
+  // Use the shared onChange function for all fields
+  function handleInputChange(event, fieldIdentifier) {
     if (onChange) {
-      console.log('event', event);
-      onChange(event?.target?.value)
+      const value = event.target.value;
+      onChange(fieldIdentifier, value);
     }
   }
 
@@ -18,14 +20,14 @@ function SendEmailComponent(props) {
       <Header generator />
       <Box className="email-content">
         <Box className="info-box">
-            <TextField label="Add your name..." className="name" />
-            <TextField label="Add your Company..." className="company" />
+          <TextField onChange={(event) => handleInputChange(event, 'name')} label="Add your name..." className="name" />
+          <TextField onChange={(event) => handleInputChange(event, 'company')} label="Add your Company..." className="company" />
         </Box>
         <Box className="email-text-box">
-            <TextField label="Add your email text..." className="email-text" />
+          <TextField onChange={(event) => handleInputChange(event, 'email-text')} label="Add your email text..." className="email-text" />
         </Box>
         <Box className="email-address-box">
-            <TextField label="Add email addresses you want to send your gif to..." className="email-address" />
+          <TextField onChange={(event) => handleInputChange(event, 'email-address')} label="Add email addresses you want to send your gif to..." className="email-address" />
         </Box>
       </Box>
       <Box className="btn-box">
@@ -33,10 +35,10 @@ function SendEmailComponent(props) {
         <Button className="btn send-email">Send email</Button>
       </Box>
       <Box className="bottom-content">
-            <Box className="go-back-content">
-                Want to create another gif? <span className="back-btn">Go back to home page here</span>
-            </Box>
-     </Box>
+        <Box className="go-back-content">
+          Want to create another gif? <span className="back-btn">Go back to the home page here</span>
+        </Box>
+      </Box>
     </div>
   );
 }
--- giff-frontend/src/context/GiftContextProvider.js
+++ giff-frontend/src/context/GiftContextProvider.js
@@ -4,12 +4,28 @@ import React, { createContext, useState } from 'react';
 export const GiftContext = createContext();
 
 const GiftContextProvider = ({ children }) => {
-  const [gifData, setGifData] = useState(null);
+  // Define a single state object to hold all the input values
+  const [inputValues, setInputValues] = useState({
+    name: '',
+    company: '',
+    emailText: '',
+    emailAddresses: '',
+  });
+
+  // Define a shared onChange function that updates the state
+  const onChange = (fieldIdentifier, value) => {
+    // Use the spread operator to update the specific field
+    setInputValues((prevInputValues) => ({
+      ...prevInputValues,
+      [fieldIdentifier]: value,
+    }));
+  };
+
   return (
     <GiftContext.Provider
       value={{
-        gifData,
-        setGifData
+        inputValues,
+        onChange,
       }}
     >
       {children}
