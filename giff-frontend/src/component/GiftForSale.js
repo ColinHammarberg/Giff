95dfc92faaa35759eb03d6048e8db7a92d046800
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-09-10 20:06:52 +0200

    update responsiveness

--- giff-frontend/src/component/GiftForSale.js
+++ giff-frontend/src/component/GiftForSale.js
@@ -46,12 +46,11 @@ const handleClick = async () => {
                 <Box>
                     <div className="title">What is Gif-t?</div>
                     <span>
-                    Gif-t for sales is all about using our gif-machine (aka the Gif-ter) to create a conversion-machine. It’s a simple way to make your <span>sales outreach</span> way, way more successful. Why? Well, do know how many sales pitches your leads get every day? It’s probably a ton and it makes it super hard to get through and be noticed. Even worse, it means that your audience may be wary of clicking links in emails and texts.<br></br><br></br>
+                        Gif-t for sales is all about using our gif-machine (aka the Gif-ter) to create a conversion-machine. It’s a simple way to make your <span>sales outreach</span> way, way more successful. Why? Well, do know how many sales pitches your leads get every day? It’s probably a ton and it makes it super hard to get through and be noticed. Even worse, it means that your audience may be wary of clicking links in emails and texts.<br></br><br></br>
 
-                    Gif-t gives you a way to pop, really. It increases the chance that your lead actually sees you and your offering. It makes it possible to stand out in a sea of boring text-based emails and messages. It boosts the chance that your lead will look at the material you share. It increases the odds that your lead will click to view whatever you want them to view etc.<br></br><br></br>
+                        Gif-t gives you a way to pop, really. It increases the chance that your lead actually sees you and your offering. It makes it possible to stand out in a sea of boring text-based emails and messages. It boosts the chance that your lead will look at the material you share. It increases the odds that your lead will click to view whatever you want them to view etc.<br></br><br></br>
 
-                    Simply create a gif from the thing you want to share in your email, like a web page, a whitepaper or a sales presentation. Then,  Share directly via  email/ sms or download to share elsewhere.  
-
+                        Simply create a gif from the thing you want to share in your email, like a web page, a whitepaper or a sales presentation. Then,  Share directly via  email/ sms or download to share elsewhere.  
                     </span>
                 </Box>
                 <Spinner onClick={handleClick} title="Content" />
