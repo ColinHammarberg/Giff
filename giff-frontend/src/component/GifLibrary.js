0254f814d1fff9281a1a40b1a00770fb4a500d87
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-10-09 18:23:24 +0200

    update gif-library

--- giff-frontend/src/component/GifLibrary.js
+++ giff-frontend/src/component/GifLibrary.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import './GifLibrary.scss';
 import Header from './Header';
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { FetchUserGifs, FetchUserInfo } from '../endpoints/Apis';
 
 function GifLibrary() {
     const [userInfo, setUserInfo] = useState(null);
+    const [selectedGif, setSelectedGif] = useState(null);
+    const gifDummyData = [
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+      {url: 'https://gift-resources.s3.eu-north-1.amazonaws.com/13/your_gift-13.gif', name: 'www.gif-t.com'},
+    ]
     useEffect(() => {
         const fetchData = async () => {
           const response = await FetchUserInfo();
@@ -18,9 +33,36 @@ function GifLibrary() {
       }, []);
     console.log('userInfo', userInfo);
   return (
-    <div className="profile">
+    <div className="gif-library">
       <Header menu />
-        <Box className="title">Profile</Box>
+      <Box className="gif-showcase">
+        <Box className="gif-showcase-info">
+          <Box className="title"><span>This is your library.</span> download all gifs at once <span>or</span> hover over the gif you want to download or share.</Box>
+          <Box className="download"><Button>Download all gifs</Button></Box>
+        </Box>
+          <Box className="gif-wrapper">
+            {gifDummyData.map((item, index) => {
+              return (
+                <Box
+                  className="gif-box" 
+                >
+                  <Box 
+                    className={`gif-container ${selectedGif === index ? 'hovered' : ''}`} 
+                    onMouseEnter={() => setSelectedGif(index)}
+                    onMouseLeave={() => setSelectedGif(null)}
+                  >
+                    <img src={item.url} alt="" />
+                    <Box className="gif-buttons">
+                      <Button className="download">Download</Button>
+                      <Button className="share">Share</Button>
+                    </Box>
+                  </Box>
+                  <span>{item.name}</span>
+                </Box>
+              )
+            })}
+          </Box>
+      </Box>
     </div>
   );
 }
