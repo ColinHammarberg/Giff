6c54c0dac74eb67830f9ba3f3e6ed7acb26cb09d
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-11-12 13:48:07 +0100

    update verify

--- giff-frontend/src/component/authorization/VerifyAccount.js
+++ giff-frontend/src/component/authorization/VerifyAccount.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { showNotification } from '../Notification';
-import { VerifyUser } from '../../endpoints/Apis';
+import { FetchUserInfo, VerifyUser } from '../../endpoints/Apis';
 import { useNavigate } from 'react-router-dom';
 import Verification from './Verifying.jpg';
 import './Verification.scss';
+import { GiftContext } from '../../context/GiftContextProvider';
 
 function VerifyAccount() {
   const navigate = useNavigate();
   const [verified, setVerified] = useState(false);
+  const { setUser } = useContext(GiftContext);
 
   useEffect(() => {
     const verifyUserAccount = async () => {
@@ -20,23 +22,36 @@ function VerifyAccount() {
       try {
         const { data, status } = await VerifyUser(token);
         if (status === 200) {
+          const access_token = localStorage.getItem('access_token');
           showNotification('success', data.status);
           setVerified(true);
-          setTimeout(() => navigate('/choose-option-create'), 5000);
+          if (access_token) {
+            const updatedUserData = await FetchUserInfo();
+            if (updatedUserData.data) {
+              setUser({
+                userInfo: updatedUserData.data,
+              });
+            }
+            setTimeout(() => navigate('/choose-option-create'), 5000);
+          } else {
+            setTimeout(() => navigate('/'), 5000);
+          }
         } else {
           showNotification('error', data.status);
         }
       } catch (error) {
         if (error.status) {
           showNotification('error', error.data.status);
+          console.log('error.data', error.data)
         } else {
           showNotification('error', error.message);
+          console.log('error.data', error)
         }
       }
     };
 
     verifyUserAccount();
-  }, [navigate]);
+  }, [navigate, setUser]);
 
   return (
     <div className="gif-landing">
