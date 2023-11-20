a07b8f8e02c9632368c4f8c9680c1b064f16ff5b
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-11-20 13:56:09 +0100

    drag and drop pdf files

--- giff-frontend/src/component/single-gif-feature/UploadPdfGifForm.js
+++ giff-frontend/src/component/single-gif-feature/UploadPdfGifForm.js
@@ -1,12 +1,38 @@
 import { TextField } from '@mui/material';
-import React, { useEffect, forwardRef, useImperativeHandle } from 'react';
+import React, { useEffect, forwardRef, useImperativeHandle, useState } from 'react';
 import { UploadPdfThenCreateGif } from '../../endpoints/Apis';
 import { showNotification } from '../notification/Notification';
 
 const UploadPdfGifForm = forwardRef(({ selectedPdf, setSelectedPdf, setIsLoading, setGifGenerated }, ref) => {
 
+  const [dragOver, setDragOver] = useState(false);
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setDragOver(true);
+  };
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    setDragOver(true);
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setDragOver(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setDragOver(false);
+    const file = e.dataTransfer.files[0];
+    if (file && file.type === "application/pdf") {
+      setSelectedPdf(file);
+    }
+  };
+
   useEffect(() => {
-    document.getElementById('custom-button').addEventListener('click', function () {
+    document.getElementById('custom-button')?.addEventListener('click', function () {
       document.getElementById('pdf-file').click();
     });
   }, []);
@@ -49,6 +75,28 @@ const UploadPdfGifForm = forwardRef(({ selectedPdf, setSelectedPdf, setIsLoading
         <label htmlFor="pdf-file" id="custom-button" className="custom-button">
           Upload pdf
         </label>
+        <div 
+          onDragOver={handleDragOver}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+          style={{ position: 'relative' }}
+        >
+          <TextField
+            disabled
+            value={selectedPdf?.name || ''}
+            placeholder="Drag and drop a PDF here or click to upload"
+          />
+          <div style={{
+            position: 'absolute', 
+            top: 0, 
+            left: 0, 
+            height: '100%', 
+            width: '100%', 
+            backgroundColor: dragOver ? 'rgba(0,0,0,0.1)' : 'transparent',
+            zIndex: dragOver ? 1 : -1,
+          }} />
+        </div>
         <input
           type="file"
           id="pdf-file"
@@ -57,7 +105,6 @@ const UploadPdfGifForm = forwardRef(({ selectedPdf, setSelectedPdf, setIsLoading
           onChange={handlePdfChange}
           style={{ display: 'none' }}
         />
-        <TextField disabled value={selectedPdf?.name} />
       </form>
     </>
   );
