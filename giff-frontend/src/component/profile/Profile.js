041aa71c834b4e9f50a5db5be2a6ad8f42b3c113
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-12-27 14:28:17 +0100

    .

--- giff-frontend/src/component/profile/Profile.js
+++ giff-frontend/src/component/profile/Profile.js
@@ -299,10 +299,6 @@ function Profile() {
             <div>Use <span>AI</span> to analyse and describe your gif while creating it</div>
             <Checkbox onChange={handleOnChangeAICheckbox} checked={aiChecked} />
           </Box>
-          <Box className="ai-checkbox">
-            <div>Use <span>AI</span> to craft an email to fit your gif while creating it</div>
-            <Checkbox onChange={handleOnChangeAICheckbox} checked={aiChecked} />
-          </Box>
         </Box>
         <Box className="delete-account">
           <Button onClick={handleOnClickDeleteAccount}>Delete Account</Button>
