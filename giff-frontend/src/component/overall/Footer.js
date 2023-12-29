d638dfec43dd43cef5d9de3d44964e01731893e9
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-12-29 15:30:20 +0100

    .

--- giff-frontend/src/component/overall/Footer.js
+++ giff-frontend/src/component/overall/Footer.js
@@ -1,16 +1,19 @@
 import { Box } from '@mui/material';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import './Footer.scss';
 
 function Footer() {
-    const navigate = useNavigate();
-    return (
-        <Box className="footer-container">
-            <Box className="gift">© Gif-t, 2023</Box>
-            <Box className="rights" onClick={() => navigate('/rights-and-privacy')}>Rights & privacy</Box>
-        </Box>
-    )
+  return (
+    <Box className="footer-container">
+      <Box className="gift">© Gif-t, 2023</Box>
+      <Box
+        className="rights"
+        onClick={() => window.open('https://gif-t.io/rights-privacy-1')}
+      >
+        Rights & privacy
+      </Box>
+    </Box>
+  );
 }
 
 export default Footer;
