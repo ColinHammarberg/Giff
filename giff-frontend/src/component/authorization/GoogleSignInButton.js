d0202b65bacb0bb554978ec7857cf97f0deda7d8
ColinHammarberg <colin.hammarberg2@gmail.com>
2024-01-03 16:24:18 +0100

    google signin button

--- giff-frontend/src/component/authorization/GoogleSignInButton.js
+++ giff-frontend/src/component/authorization/GoogleSignInButton.js
@@ -1,24 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { GoogleSignUp } from '../../endpoints/UserEndpoints';
 import { showNotification } from '../notification/Notification';
 
 const GoogleSignInButton = ({ handleSignUpResponse }) => {
-  const GOOGLE_CLIENT_ID = '780954759358-cqnev3bau95uvbk80jltofofr4qc4m38.apps.googleusercontent.com';
+  const GOOGLE_CLIENT_ID =
+    '780954759358-cqnev3bau95uvbk80jltofofr4qc4m38.apps.googleusercontent.com';
 
-  const handleGoogleResponse = async (response) => {
-    try {
-      const googleResponse = await GoogleSignUp({ token: response.credential });
-      handleSignUpResponse(googleResponse);
-    } catch (error) {
-      showNotification('error', 'Google signup failed');
-    }
-  };
+  const handleGoogleResponse = useCallback(
+    async (response) => {
+      try {
+        const googleResponse = await GoogleSignUp({
+          token: response.credential,
+        });
+        handleSignUpResponse(googleResponse);
+      } catch (error) {
+        showNotification('error', 'Google signup failed');
+      }
+    },
+    [handleSignUpResponse]
+  );
 
   useEffect(() => {
     window.google.accounts.id.initialize({
       client_id: GOOGLE_CLIENT_ID,
-      callback: response => handleGoogleResponse(response),
+      callback: (response) => handleGoogleResponse(response),
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [handleGoogleResponse]);
 
   const signInWithGoogle = () => {
