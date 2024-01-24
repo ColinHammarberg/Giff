ccef5e00241a0a89167ca0a2b8864459306534d1
ColinHammarberg <colin.hammarberg2@gmail.com>
2024-01-24 21:41:18 +0100

    .

--- giff-frontend/src/tracking/ValidateGifSource.js
+++ giff-frontend/src/tracking/ValidateGifSource.js
@@ -7,17 +7,16 @@ function ValidateGifSource() {
   const { gif_id } = useParams();
   const { mutate: trackClick } = useTrackGifClick();
 
+  console.log('GIF ID:', gif_id);
+
   React.useEffect(() => {
     if (gif_id) {
+      console.log('GIF ID2:', gif_id);
       trackClick(gif_id);
     }
   }, [gif_id, trackClick]);
 
-  return (
-    <div className="gif-validator">
-      Testing
-    </div>
-  );
+  return <div className="gif-validator">Testing</div>;
 }
 
 export default ValidateGifSource;
