e05b6aaefc205fe2045957f10189fcb02383ea0f
ColinHammarberg <colin.hammarberg2@gmail.com>
2024-02-17 11:00:29 +0100

    update

--- test/include_ai_helper.py
+++ test/include_ai_helper.py
@@ -13,7 +13,6 @@ def include_ai_in_gifs():
     if user:
         # Extract the boolean value from the nested dictionary
         include_ai_value = data.get('include_ai', {}).get('includeAI')
-        print('include_ai_value', include_ai_value)
         
         if isinstance(include_ai_value, bool):
             user.include_ai = include_ai_value
@@ -26,14 +25,29 @@ def include_ai_in_gifs():
         return jsonify({"status": "error", "message": "User not found"}), 404
 
 @jwt_required()
+def add_watermark():
+    user_id = get_jwt_identity()
+    data = request.get_json()
+    user = User.query.filter_by(id=user_id).first()
+
+    if user:
+        # Directly get the boolean value for add_watermark
+        add_watermark = data.get('add_watermark')
+
+        # Ensure add_watermark is a boolean before proceeding
+        if isinstance(add_watermark, bool):
+            user.watermark = add_watermark
+            db.session.commit()
+            return jsonify({"status": "success", "data": {"watermark": user.watermark}}), 200
+        else:
+            return jsonify({"status": "error", "message": "Invalid value for add_watermark"}), 400
+@jwt_required()
 def include_ai_email():
     user_id = get_jwt_identity()
     user = User.query.filter_by(id=user_id).first()
     data = request.get_json()
-    print('data', data)
     if user:
         include_example_email = data.get('include_example_email', {}).get('includeExampleEmail')
-        print('include_example_email', include_example_email)
         user.include_example_email = include_example_email
         db.session.commit()
         return jsonify({"status": "success", "data": user.include_example_email}), 200
